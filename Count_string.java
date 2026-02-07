dabfd22bbe3b875e118fb0eb1acadd6ffa5eaeb8
dipti58 <biswaldipti757@gmail.com>
2026-02-07 23:41:40 +0530

    Add files via upload

---
+++ Count_string.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Count_string {
+    public static void main(String[] args) {
+       String str="lipi";
+       int count=0;
+       for(int i=0; i<str.length(); i++){
+          count++;
+       }
+       System.out.println(count);
+    }
+}
+
